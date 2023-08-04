2b430530041abd80b7c67b1c31713484972e4fec
Philip Squire <kpsquire@gmail.com>
2023-08-04 11:31:39 -0400

    Updated information, reformated architecture

--- button.js
+++ button.js
@@ -1,6 +1,11 @@
 const BackToTopButton = document.getElementById('BackToTop');
+const ReturnToProjectsButton = document.getElementById('ReturnToProjects');
 
 BackToTopButton.addEventListener('click', function () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+});
+
+ReturnToProjectsButton.addEventListener('click', function () {
+  window.location.href = "/projects/";
 });
